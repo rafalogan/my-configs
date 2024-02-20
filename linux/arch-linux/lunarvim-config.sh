35a7cd379298a3ab3d1812a318fe323369098397
Rafael Candeira <www.logan.resident@gmail.com>
2024-02-20 17:18:44 -0300

    update fonts

--- linux/arch-linux/lunarvim-config.sh
+++ linux/arch-linux/lunarvim-config.sh
@@ -2,8 +2,15 @@
 #
 # update Arch Linux
 yay -Syu --noconfirm && yay -Yc --noconfirm &&
+# install firaCode
+yay -S --noconfirm ttf-fira-code &&
+# install nerd-fonts
+yay -S --noconfirm nerd-fonts-complete &&
+# install mesloLGS
+yay -S --noconfirm ttf-meslo-nerd-font-powerlevel10k &&
 # install lunarvim
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) &&
 # update zsh
 source ~/.zshrc &&
+lvim +LvimUpdate +q &&
 echo("LunarVim installed")
